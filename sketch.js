66421f9c290d3a9cbdcbd4f30f92fb713f9d8880
unknown <mubaraqkazeem@gmail.com>
2021-03-15 18:37:25 -0400

    added text

--- sketch.js
+++ sketch.js
@@ -40,6 +40,7 @@ function setup(){
 
 function draw(){
     background(backgroundImg);
+    text ("angrybird game ",200,100)
     Engine.update(engine);
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
